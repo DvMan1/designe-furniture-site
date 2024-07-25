335bcfb98a8fd2f9990f7cca86677995529c3ad1
Egor <msubzero2001@gmail.com>
2024-07-25 19:10:11 +0300

    change_coment

--- src/components/Pages/Home/Comments/Comment/Comment.jsx
+++ src/components/Pages/Home/Comments/Comment/Comment.jsx
@@ -1,8 +1,8 @@
 import styles from "./Comment.module.scss"
 
-const Comment =({name,text})=>{
-    return(<>
+const Comment =({className,name,text})=>{
+    return(<div className={className}>
     <h2 className={styles.comment_title}>Name:{name}<p className={styles.comment_text}>{text}</p></h2>
-    </>)
+    </div>)
 }
 export default Comment
