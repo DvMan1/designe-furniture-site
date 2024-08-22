0cd3905267191e6336e74525e094d6c6e6f3bf2d
Egor <msubzero2001@gmail.com>
2024-08-22 21:02:40 +0300

    change_form

--- src/components/Pages/Home/FormCallback/FormCallback.jsx
+++ src/components/Pages/Home/FormCallback/FormCallback.jsx
@@ -4,10 +4,12 @@ import Form from "./Form/Form";
 const FormCallback = () => {
   return (
     <div className={styles.form_container}>
-      <h2 className={styles.form_title}>
-        ПРЕКРАСНАЯ КОНСУЛЬТАЦИЯ ДЛЯ БАГАТЫХ ЛЮДЕЙ С МАССАЖЕМ СТУПНЕЙ ГЕМОРРОЕМ{" "}
-      </h2>
-      <Form />
+      <div className={styles.form_box}>
+        <h2 className={styles.form_title}>
+          ПРЕКРАСНАЯ КОНСУЛЬТАЦИЯ ДЛЯ БАГАТЫХ ЛЮДЕЙ С МАССАЖЕМ СТУПНЕЙ ГЕМОРРОЕМ{" "}
+        </h2>
+        <Form />{" "}
+      </div>
     </div>
   );
 };
